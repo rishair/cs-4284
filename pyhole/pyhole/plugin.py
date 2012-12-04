4f2e8032e239d8f59f1825e739e9d84654c95a4d
Rishi Ishairzay <windowasher@gmail.com>
2012-12-04 13:54:10 -0500

    Summarizer with hash automatically prepended

--- pyhole/pyhole/plugin.py
+++ pyhole/pyhole/plugin.py
@@ -110,15 +110,6 @@ class Plugin(object):
         self.irc = irc
         self.name = self.__class__.__name__
 
-    def extract_command(self, kwargs):
-        print kwargs["full_message"] + "---"
-        match = re.search("\$([ -~]+)\$", kwargs["full_message"])
-        hash = ""
-        if match:
-            hash = match.group(1)
-        message = re.sub("\$([ -~]+)\$", "", kwargs["full_message"])
-        return (message, hash)
-
 def _init_plugins(*args, **kwargs):
     """Create instances of the plugin classes and create a cache
     of their hook functions
--- pyhole/pyhole/plugins/bot_commands.py
+++ pyhole/pyhole/plugins/bot_commands.py
@@ -16,6 +16,6 @@ class BotCommands(plugin.Plugin):
     @plugin.hook_add_command("bot")
     def bot(self, params=None, **kwargs):
         """Test out the server-side summary using non-local sending"""
-        (message, hash) = self.extract_command(kwargs)
+        (message, hash) = self.irc.extract_command(kwargs["full_message"])
         self.irc.reply(str(random.randint(0, 10)), hash)
 
