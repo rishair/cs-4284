1c9e38c87e9bff0c696d844122f5bb82cba5bea4
Carrotman42 <kevin@chowski.com>
2012-12-14 21:37:17 -0500

    Trimmed unused files and updated the readme
    These files are unused. If we need them they are in the history.

--- pyhole/pyhole/plugins/summary_test.py
+++ pyhole/pyhole/plugins/summary_test.py
@@ -16,7 +16,6 @@ class SummaryTest(plugin.Plugin):
     @plugin.hook_add_command("sum")
     def summaryTest(self, params=None, **kwargs):
         """Test out the client-side summary using non-local sending"""
-        self.irc.reply(str(random.randint(0, 4)))
         # Simulate actual data to aggregate, send to the reply channel
-        # self.irc.reply("sum;%s;%s" % (hash(kwargs["full_message"] + self.irc.source), random.randint(0,4)))
+        self.irc.reply("sum;%s;%s" % (hash(kwargs["full_message"] + self.irc.source), random.randint(0,4)))
 
