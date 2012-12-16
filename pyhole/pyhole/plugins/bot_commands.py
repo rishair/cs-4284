6618d74c82c7283512049be271a845aee6f55598
Rishi Ishairzay <windowasher@gmail.com>
2012-12-15 23:13:11 -0500

    Fixed botcommands

--- pyhole/pyhole/plugins/bot_commands.py
+++ pyhole/pyhole/plugins/bot_commands.py
@@ -16,6 +16,5 @@ class BotCommands(plugin.Plugin):
     @plugin.hook_add_command("bot")
     def bot(self, params=None, **kwargs):
         """Test out the server-side summary using non-local sending"""
-        (message, hash) = self.irc.extract_command(kwargs["full_message"])
-        self.irc.reply(str(random.randint(0, 10)), hash)
+        self.irc.reply(str(random.randint(0, 10)))
 
